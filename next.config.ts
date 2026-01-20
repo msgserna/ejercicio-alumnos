9b355a389c07ac6195fdc11c6758e67a7fe2e0d8
msgserna <mgarzons@student.ifp.es>
2026-01-20 10:53:50 +0100

    deploy pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/next-app",
+  assetPrefix: "/next-app/",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
